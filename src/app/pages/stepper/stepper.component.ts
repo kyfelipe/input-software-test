4d9790bc018d26f43f7464a4baefe760d47f7ad0
Felipe Côrtes <contatos.cortes@gmail.com>
2019-09-10 18:05:55 -0300

    + Criado o formulado do segundo step; + Validado o primeiro e segundo campo; + Bloqueado avançado do primeiro para o segundo step caso o primeiro campo esteja inválido.

--- src/app/pages/stepper/stepper.component.ts
+++ src/app/pages/stepper/stepper.component.ts
@@ -7,7 +7,9 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
     styleUrls: ['./stepper.component.scss']
 })
 export class StepperComponent implements OnInit {
-    firstFormGroup: FormGroup;
+    public firstFormGroup: FormGroup;
+    public secondFormGroup: FormGroup;
+    private previousValueFirstCtrl: number;
 
     constructor(private formBuilder: FormBuilder) { }
 
@@ -15,5 +17,40 @@ export class StepperComponent implements OnInit {
         this.firstFormGroup = this.formBuilder.group({
             firstCtrl: ['', Validators.required]
         });
+
+        this.secondFormGroup = this.formBuilder.group({
+            secondCtrl: ['', Validators.required]
+        });
+    }
+
+    get firstCtrl() {
+        return this.firstFormGroup.get('firstCtrl').value;
+    }
+
+    get secondCtrl() {
+        return this.secondFormGroup.get('secondCtrl').value;
+    }
+
+    public updatePreviousValueFirstCtrl() {
+        if (this.previousValueFirstCtrl !== this.firstCtrl) {
+            this.secondFormGroup.reset();
+            this.secondFormGroup.get('secondCtrl').clearValidators();
+        }
+        this.previousValueFirstCtrl = this.firstCtrl;
+    }
+
+    public secondCtrlIsValid() {
+        if (this.secondFormGroup.get('secondCtrl').valid) {
+            if (this.secondCtrl) {
+                if (parseInt(this.firstCtrl) === this.secondCtrl.length) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public amountRequiredNextStep() {
+        return parseInt(this.firstCtrl) - (this.secondCtrl ? this.secondCtrl.length : 0);
     }
 }
